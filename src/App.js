e4aca1f2003dac401ed02defad897199f7b5423e
Sarah Thomas <sbade92@gmail.com>
2023-07-06 21:54:47 -0700

    Started working on app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import Confetti from "react-confetti";
 
 function App() {
   return (
     <div className="App, App-header">
-      <Confetti />
-      <h1>Wow I'm learning React</h1>
-      <p>Happiness?</p>
     </div>
   );
 }
---
+++ src/questionList.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const questionList = [
+    {
+        question: "Is the floor cluttered?",
+        answerYes: "Yes",
+        answerNo: "No",
+        actionItem: "Pick up any clutter."
+    },
+    {
+        question: "Are there any rugs on the floor?  If so, are the rugs loose?",
+        answerYes: "Yes",
+        answerNo: "No",
+        actionItem: "Remove rugs or secure them with double-sided tape or non-slip backing."
+    }, 
+    {
+        question: "Is the bathroom missing grab bars next to and inside the tub, and next to the toilet?",
+        answerYes: "Yes",
+        answerNo: "No",
+        actionItem: "Install grab bars in the bathroom next to and inside the tub, and next to the toilet."
+    },
+]
+
+
+
+Are any staircases poorly lit or missing a handrail?  Is there a handrail on only one side of the stairs? Is there only one light switch for the stairs?
+Yes
+No 
+“Have a light switch installed both at the top and bottom of the stairs.  Install handrails on both sides of the stairs.”
+Is the home poorly lit?
+Yes
+No 
+“Add lamps or overhead lighting until the home is well-lit.”
+Is anything in the home in obvious disrepair?  This could include loose or torn carpets, broken handrails or steps, or burned-out lightbulbs.  
+Yes
+No 
+“Repair or replace any broken steps or handrails.  Change out burned-out lightbulbs.  Replace torn carpet.”
+Is any furniture blocking the main pathways of the home?
+Yes
+No 
+“Move furniture so that the home’s main pathways are clear.”
+Are there any loose wires or cords you have to go around or step over? 
+Yes
+No 
+“Tape wires and cords to the wall, or at least move them against the wall where they aren’t a tripping hazard.”
+Is anything stored in high cupboards or shelves?
+Yes
+No 
+“Store frequently-used items at waist height.”
+Is the nearest light source (lamp or light switch) out of reach from the bed? Is the path between the bedroom and bathroom poorly lit? 
+Yes
+No 
+“Make sure there’s a lamp or light switch within easy reach of the bed.  Install a nightlight to make sure the path from the bed to the bathroom is well-lit.
+Is it slippery in the tub or shower?
+Yes
+No 
+“Place self-stick strips or a non-slip rubber mat on the floor of the tub or shower.”
+
+export default questionList; 
+
