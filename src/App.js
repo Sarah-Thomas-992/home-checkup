7c5a2bf81c177ceec80d769a58a554a61319102e
Sarah Thomas <sbade92@gmail.com>
2023-07-06 21:32:49 -0700

    Played with confetti

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Confetti from "react-confetti";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App, App-header">
+      <Confetti />
+      <h1>Wow I'm learning React</h1>
+      <p>Happiness?</p>
     </div>
   );
 }
